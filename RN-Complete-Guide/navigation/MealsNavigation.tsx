7bf0d676ffaa193511c628edaf52ba5a865868de
tiennt99 <Honda30y15454>
2021-04-27 23:57:01 +0700

    128

--- RN-Complete-Guide/navigation/MealsNavigation.tsx
+++ RN-Complete-Guide/navigation/MealsNavigation.tsx
@@ -4,13 +4,35 @@ import MealDetailScreen from '../screens/MealDetailScreen';
 import { createStackNavigator } from '@react-navigation/stack';
 import { NavigationContainer } from '@react-navigation/native';
 import * as React from 'react';
+import Color from '../constants/Colors'
+import { Platform } from 'react-native';
 const Stack = createStackNavigator();
 export default function BottomTabNavigator() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="Categories" component={CategoriesScreen} />
-        <Stack.Screen name="CategoryMeal" component={CategoryMealsScreen} />
+        <Stack.Screen 
+          name="Categories" 
+          component={CategoriesScreen} 
+          options={{ 
+            title: 'Meal Categories',
+            headerStyle:{
+              backgroundColor:Platform.OS === 'android' ? Color.primaryColor : 'white'
+            },
+            headerTintColor: Platform.OS === 'android' ? 'white' : Color.titleColor
+          }}
+        />
+        <Stack.Screen 
+          name="CategoryMeal" 
+          component={CategoryMealsScreen} 
+          options={({ route }) => ({ 
+            title: route.params?.title,
+            headerStyle:{
+              backgroundColor:Platform.OS === 'android' ? Color.primaryColor : 'white'
+            },
+            headerTintColor: Platform.OS === 'android' ? 'white' : Color.titleColor
+          })}
+          />
         <Stack.Screen name="MealDetail" component={MealDetailScreen} />
       </Stack.Navigator>
     </NavigationContainer>
